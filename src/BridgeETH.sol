e98b2b1f58ee1aadfc10aec5c8f20ea569463361
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-02 00:34:14 +0530

    update BridgeETH.sol

--- src/BridgeETH.sol
+++ src/BridgeETH.sol
@@ -8,5 +8,24 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract BridgeETH { 
-    //hi there 
+    uint256 public balance;
+    address tokenAddress;
+    mapping(address => uint256) pendingBalance;
+
+    constructor(address _tokenAddress) Ownable(msg.sender) {
+        tokenAddress = _tokenAddress;
+    }
+
+    function deposit(IERC20 _tokenAddress, uint256 _amount) public {
+        require(address(_tokenAddress) == tokenAddress);
+        require(_tokenAddress.allowance(msg.sender, address(this)) >= _amount);
+        _tokenAddress.transferFrom(msg.sender, address(this), _amount);
+        pendingBalance[msg.sender] += _amount;
+    }
+
+    function withdraw(IERC20 _tokenAddress, uint256 _amount) public {
+        require(pendingBalance[msg.sender] >= _amount);
+        pendingBalance[msg.sender] -= _amount;
+        _tokenAddress.transfer(msg.sender, _amount);
+    }
 }
