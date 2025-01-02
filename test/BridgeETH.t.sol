3cb5f124a363ce968a701db64c841d82e8beab52
Ashiq Ahamed <aashiq121205@gmail.com>
2025-01-02 16:14:07 +0530

    update test_Deposit

--- test/BridgeETH.t.sol
+++ test/BridgeETH.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 
 import "src/BridgeETH.sol";
 import "src/USDT.sol";
+
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
@@ -22,4 +23,22 @@ contract BridgeETHTest is Test {
     function testTokenName() public {
         assertEq(usdt.name(), "USDT", "Token name mismatch");
     }
+
+    function test_Deposit() public {
+        address testAddr = vm.addr(1);
+        usdt.mint(testAddr, 200);
+
+        vm.startPrank(testAddr);
+        usdt.approve(address(bridge), 200);
+        bridge.deposit(usdt, 200);
+
+        assertEq(usdt.balanceOf(testAddr), 0);
+        assertEq(usdt.balanceOf(address(bridge)), 200);
+
+        bridge.withdraw(usdt, 100);
+
+        assertEq(usdt.balanceOf(testAddr), 100);
+        assertEq(usdt.balanceOf(address(bridge)), 100);
+    }
+
 }
